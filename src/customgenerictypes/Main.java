e7fe67fdd7610e5014f45b759f6ff60bee361b8c
ATixonov <tixxxonnnofff@rambler.ru>
2022-10-07 18:13:52 +0300

    refactored generic types

--- src/customgenerictypes/Main.java
+++ src/customgenerictypes/Main.java
@@ -1,4 +1,12 @@
 package customgenerictypes;
 
 public class Main {
+    public static void main(String[] args){
+        Account<Integer> acc1 = new Account(13, 500);
+        System.out.println(acc1.getId());
+        Account<String> acc2 = new Account("Gena228", 300);
+        System.out.println(acc2.getId());
+        int a = acc1.getId() + 10;
+        System.out.println(a);
+    }
 }
